398117d993dea74874e9d7e6b5cd325dc79daa5e
wilfredo-domanico-jr <wilfredo.domanico.jr@gmail.com>
2023-10-10 13:02:42 +0800

    Created Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            Home
+    <section class="flex mt-8 mb-8 mr-12 ml-12 justify-center grid grid-cols-4 gap-4">
+
+
+        <div class="col-span-3 h-screen">
+            <div class="h-96 mb-8">
+                <img src="{{ asset('images/profile-bg.jpg') }}" class="w-full h-full rounded-lg">
+            </div>
+ 
+            <div class="grid grid-cols-3 gap-4 h-auto">
+
+                <div class="flex flex-col h-auto gap-12">
+
+                    <div class="min-h-[400px] bg-white rounded-xl text-2xl border-solid border-2 border-gray-600">
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <i class="text-6xl mr-2 fa-regular fa-envelope"></i>
+                            <div class="flex flex-end ml-8">
+
+                                <span>Invitation Request</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">5</span>
+
+                            </div>
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <i class="text-6xl mr-2 fa-solid fa-user-lock"></i>
+                            <div class="flex ml-4">
+                                <span>Blocked Accounts</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">0</span>
+                            </div>
+
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+
+                            <i class="text-6xl mr-2 fa-solid fa-circle-plus"></i>
+
+                            <span class="ml-8">Create Page</span>
+
+
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-gray-600">
+
+                            <i class="text-6xl mr-2 fa-solid fa-circle-plus"></i>
+                            <span class="ml-8">Create Group</span>
+
+                        </section>
+
+
+
+                    </div>
+
+                    <div class="min-h-[600px] bg-white rounded-xl text-2xl border-solid border-2 border-gray-600">
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <div class="flex flex-end ml-8">
+
+                                <span>Most Trending</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">5</span>
+
+                            </div>
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <div class="flex flex-end ml-8">
+
+                                <span>Most Trending</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">5</span>
+
+                            </div>
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <div class="flex flex-end ml-8">
+
+                                <span>Most Trending</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">5</span>
+
+                            </div>
+                        </section>
+
+                        <section class="flex items-center h-1/4 p-6 border-solid border-b-2 border-gray-600">
+                            <div class="flex flex-end ml-8">
+
+                                <span>Most Trending</span>
+                                <span class="flex text-gray text-center border-solid border-gray-950 ml-4">5</span>
+
+                            </div>
+                        </section>
+                    </div>
+
+
+                </div>
+
+                <div class="col-span-2 h-96 bg-purple-200">
+
+                </div>
+
+            </div>
+
         </div>
-    </div>
+        <div class="flex flex-col bg-blue-200 h-auto gap-12">
+
+            <div class="h-96 bg-red-200 rounded-md"></div>
+            <div class="h-96 bg-green-200 rounded-md"></div>
+
+        </div>
+
+    </section>
+
 @endsection
+
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,28 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Posty</title>
-
+        <title>Social App</title>
+        <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+        <script>
+            tailwind.config = {
+              theme: {
+                extend: {
+                  colors: {
+                    clifford: '#da373d',
+                  }
+                }
+              }
+            }
+          </script>
     </head>
-    <body class="bg-gray-200">
-        <nav class="p-6 bg-white flex justify-between mb-6">
-            <ul class="flex items-center">
+    <body class="bg-slate-100">
+        <nav class="p-6 bg-amber-500 flex justify-between mb-6">
+
+            <span><img src="{{ asset('images/logo.svg') }}" class="w-28"></span>
+
+            {{-- <ul class="flex items-center text-white">
                 <li>
                     <a href="/" class="p-3">Home</a>
                 </li>
@@ -20,19 +35,42 @@
                 <li>
                     <a href="{{ route('posts') }}" class="p-3">Posts</a>
                 </li>
-            </ul>
-
-            <ul class="flex items-center">
+            </ul> --}}
+            <form class="flex w-1/3 text-gray-400">
+            <input type="search" class="w-full p-4 rounded-full" placeholder="Search">
+            {{-- <button type="submit" style=""><i class="fa-solid fa-magnifying-glass"></i></button> --}}
+            </form>
+            <ul class="flex items-center text-white text-3xl w-96 justify-evenly">
                 @auth
                     <li>
-                        <a href="" class="p-3">{{ auth()->user()->name }}</a>
+                        <a href="/" class="p-3"><i class="fa-solid fa-house"></i></a>
+                        {{-- <a href="" class="p-3">{{ auth()->user()->name }}</a> --}}
                     </li>
-                    <li>
+                    {{-- <li>
                         <form action="{{ route('logout') }}" method="post" class="p-3 inline">
                             @csrf
                             <button type="submit">Logout</button>
                         </form>
+                    </li> --}}
+
+                    <li>
+                        <a href="/" class="p-3"><i class="fa-solid fa-bell"></i></a>
                     </li>
+
+                    <li>
+                        <a href="/" class="p-3"><i class="fa-solid fa-gear"></i></a>
+                    </li>
+
+                    <li>
+                        <img src="{{ asset('images/profile.png') }}" class="w-14">
+                    </li>
+
+                    {{-- <li>
+                        <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('posts') }}" class="p-3">Posts</a>
+                    </li> --}}
                 @endauth
 
                 @guest
@@ -46,5 +84,12 @@
             </ul>
         </nav>
         @yield('content')
+
+
     </body>
 </html>
+
+{{--
+REFERENCE:
+
+https://www.behance.net/gallery/93022203/Social-media-UIUX-design-Ajwbetcom-website --}}
